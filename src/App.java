7cc8979c2df612db968512d56590e9d109c8ed76
Parne92 <nathan_parnell3@hotmail.com>
2022-11-30 17:32:25 -0700

    File Formatting.

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
          * Manual vertex cover verifier
          * Provide file name of graph and list of vertices seperated by commas
          */
-        Graph graph = new Graph("graph6.txt"); // graph
+        Graph graph = new Graph("graph1.txt"); // graph
         int[] vcTest = {1,4,5,7,9}; // vertex cover to test
         GraphToolBox.isVC(graph, vcTest);
 
