94d63c6417d9f24f0075d9a098427db8781e96f2
Parne92 <nathan_parnell3@hotmail.com>
2022-11-30 17:29:43 -0700

    Added Graphs, Working with IntelliJ now.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         /*
          * Manual vertex cover verifier
          * Provide file name of graph and list of vertices seperated by commas
